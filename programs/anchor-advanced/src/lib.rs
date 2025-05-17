b8aee7f15e0c6f1246669fbda88f722e0b56b7bb
AuroraLantean <raymondlieu.crypto@runbox.com>
2025-05-17 12:48:41 +0300

    update dependencies

--- programs/anchor-advanced/src/lib.rs
+++ programs/anchor-advanced/src/lib.rs
@@ -5,12 +5,12 @@ declare_id!("8WztRFKZTfdSU2J7zGX9JpGDBpBqFqEMjNRQRW9SPRSJ");
 
 #[program]
 pub mod anchor_advanced {
-    use super::*;
+  use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
+  pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    msg!("Greetings from: {:?}", ctx.program_id);
+    Ok(())
+  }
 }
 
 #[derive(Accounts)]
